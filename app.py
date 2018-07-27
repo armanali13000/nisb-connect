a0e6f4500dac5b50a03be352b88b143b601ef343
mridulganga <mridul.kepler@gmail.com>
2018-07-27 19:28:06 +0530

    init commit

---
+++ app.py
@@ -0,0 +1,274 @@
+from flask import Flask, g, render_template, flash, redirect, url_for, abort, session, request, make_response
+from flask_bcrypt import check_password_hash
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from db import *
+import datetime
+app = Flask(__name__)
+app.secret_key = 'DevangIsTheGreatest'
+
+
+# MAIN POSTS and SINGLE POST
+
+@app.route('/')
+def index():
+    if session.get("login"):
+        return render_template('index.html',allposts = post_list(),fposts=post_list_follow(str(session["user_id"])))
+    else:
+        print request.cookies
+        if request.cookies.get('login')=='1':
+            session["login"]=True
+            session["user_id"] = request.cookies.get('user_id')
+            session["username"] = request.cookies.get('username')
+            session["email"] = request.cookies.get('email')
+            return redirect('/')
+        return redirect('/login')
+
+
+@app.route('/single/<post_id>')
+@app.route('/post/single/<post_id>')
+def single(post_id):
+    return render_template('single.html',post = get_post(post_id)[0])
+
+
+
+# REGISTER LOGIN LOGOUT
+
+@app.route('/register', methods=["POST","GET"])
+def register_page():
+    if request.method=='POST':
+        username=request.form["username"]
+        password=request.form["password"]
+        email=request.form["email"]
+        bio=request.form["bio"]
+        dob=request.form["dob"]
+        ieee_no=request.form["ieee_no"]
+        branch=request.form["branch"]
+        sem=request.form["sem"]
+        if username and password and email and bio and dob and ieee_no and branch and sem:
+            user_register(username,password,email,bio,dob,ieee_no,branch,sem)
+            return redirect('/login')
+        else:
+            return render_template('register.html',err="Please Fill all stuff")
+    else:
+        return render_template('register.html')
+
+@app.route('/login', methods=["POST","GET"])
+def login_page():
+    if request.method=='POST':
+        result = user_login(request.form["email"],request.form["password"])
+        if result:
+            session["login"] = True
+            session["user_id"] = result[0]
+            session["username"] = result[1]
+            session["email"] = result[3]
+            resp = make_response(redirect('/'))
+            expire_date = datetime.datetime.now()
+            expire_date = expire_date + datetime.timedelta(days=90)
+            resp.set_cookie('user_id', str(result[0]), expires=expire_date)
+            resp.set_cookie('username', result[1], expires=expire_date)
+            resp.set_cookie('email', result[3], expires=expire_date)
+            resp.set_cookie('login','1', expires=expire_date)
+            return resp
+        else:
+            return render_template('login.html',err='You are doing it wrong pal.')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session["login"] = False
+    session["user_id"] = ""
+    session["username"] = ""
+    session["email"] = ""
+    resp = make_response(render_template('login.html'))
+    resp.set_cookie('login','0')
+    return resp
+
+
+
+
+# MEMBERS FUNCTIONS
+
+
+@app.route('/members/all')
+def member_page():
+    result = members_list()
+    return render_template('members.html', members=result)
+
+@app.route('/members/followers')
+def member_followers_page():
+    result = follower_list(str(session['user_id']))
+    return render_template('members.html', members=result)
+
+@app.route('/members/following')
+def member_following_page():
+    result = following_list(str(session['user_id']))
+    return render_template('members.html', members=result)
+
+@app.route('/profile')
+def profile_page():
+    num_followers = len(follower_list(session["user_id"]))
+    num_following = len(following_list(session["user_id"]))
+    return render_template('profile.html',member = member_info(session["user_id"]),cfollower=num_followers,cfollowing =num_following)
+
+@app.route('/profile/<user_id>')
+def profile_page_user(user_id):
+    num_followers = len(follower_list(user_id))
+    num_following = len(following_list(user_id))
+    return render_template('profile.html',member = member_info(user_id),cfollower=num_followers,cfollowing =num_following)
+
+@app.route('/userposts/<user_id>')
+def user_posts_page(user_id):
+    return render_template('user_posts.html',allposts = post_list_user(user_id))
+
+@app.route('/settings', methods=["POST","GET"])
+def settings_page():
+    if request.method=="POST":
+        username=request.form["username"]
+        email=request.form["email"]
+        bio=request.form["bio"]
+        dob=request.form["dob"]
+        ieee_no=request.form["ieee_no"]
+        branch=request.form["branch"]
+        sem=request.form["sem"]
+        user_update(username,email,bio,dob,ieee_no,branch,sem)
+        return redirect('/profile')
+    return render_template('settings.html',member=member_info(session["user_id"]))
+
+
+
+# MESSAGING
+
+@app.route('/message/<user_id>',methods=["POST","GET"])
+def message_page(user_id):
+    if request.method=="POST":
+        send_message(session["user_id"],user_id,request.form["message"])
+    return render_template('message.html',user_id=user_id)
+
+@app.route('/message_list/<user_id>',methods=["POST","GET"])
+def message_list_page(user_id):
+    messages = get_messages(session["user_id"],user_id)
+    return render_template('message_list.html',messages=messages)
+
+@app.route('/message_clear/<user_id>')
+def message_clear_page(user_id):
+    messages = clear_messages(session["user_id"],user_id)
+    return redirect( request.referrer)
+
+
+# POST add edit delete
+
+@app.route('/addpost',methods=["GET","POST"])
+def add_user_post():
+    if request.method=='POST':
+        id=str(session["user_id"])
+        post_data= request.form["post_data"]
+        wgroup = request.form["group"]
+        add_post(id,post_data,wgroup)
+        return redirect( request.referrer)
+    return render_template('index.html')
+
+@app.route('/editpost/<post_id>',methods=["GET","POST"])
+def edit_user_post(post_id):
+    if request.method=="POST":
+        post_id = request.form["post_id"]
+        edit_post(request.form["post_data"],post_id)
+        return redirect('/single/'+post_id)
+    else:
+        return render_template('edit_post.html',post=get_post(post_id)[0])
+
+@app.route('/deletepost/<post_id>')
+def delete(post_id):
+    remove_post(post_id)
+    return redirect( request.referrer)
+
+
+
+
+@app.route('/group/list')
+def view_groups_list():
+    return render_template('groups_list.html',groups=get_groups_list())
+
+# Group Stream
+@app.route('/group/<group_id>')
+def view_groups_stream(group_id):
+    g_info = get_group_info(group_id)
+    if g_info:
+        return render_template('group_stream.html',group=g_info,allposts=group_post_list(group_id))
+    else:
+        return redirect('/group/list')
+
+@app.route('/group/join/<group_id>')
+def join_group_page(group_id):
+    user_id = session["user_id"]
+    join_group(group_id,user_id)
+    return redirect(request.referrer)
+
+@app.route('/group/leave/<group_id>')
+def leave_group_page(group_id):
+    user_id = session["user_id"]
+    if leave_group(group_id,user_id)==0:
+        return redirect('/group/list')
+    return redirect(request.referrer)
+
+@app.route('/group/create', methods=["GET","POST"])
+def create_group_page():
+    if request.method=="POST":
+        rid = create_group(request.form["name"],request.form["desc"],session["user_id"])
+        return redirect('/group/'+rid)
+    else:
+        return render_template('group_create.html')
+
+@app.route('/group/members/<group_id>')
+def group_members_page(group_id):
+    members=[]
+    for x in get_group_members(group_id):
+        members.append(member_info(x))
+    print members
+    return render_template('members.html', members=members)
+
+
+
+
+
+
+# Redirecting Functions
+
+@app.route('/follow/<user_id>')
+def follow_page(user_id):
+    to_id= user_id
+    from_id=session["user_id"]
+    follow(from_id,to_id)
+    return redirect( request.referrer)
+
+@app.route('/unfollow/<user_id>')
+def unfollow_page(user_id):
+    to_id= user_id
+    from_id=session["user_id"]
+    unfollow(from_id,to_id)
+    return redirect( request.referrer)
+
+
+
+@app.route('/like/<post_id>')
+def like(post_id):
+    user_id=str(session["user_id"])
+    like_post(user_id,post_id)
+    return redirect( request.referrer)
+
+@app.route('/unlike/<post_id>')
+def unlike(post_id):
+    user_id=session["user_id"]
+    unlike_post(user_id,post_id)
+    return redirect( request.referrer)
+
+
+
+
+
+
+if __name__ == '__main__':
+    # session["login"] = False
+    app.jinja_env.globals.update(is_liked=is_liked)
+    app.jinja_env.globals.update(get_likes=get_likes)
+    app.jinja_env.globals.update(is_following=is_following)
+    app.run(host='0.0.0.0', port=8000, debug=True)
---
+++ db.py
@@ -0,0 +1,411 @@
+# import mysql.connector
+
+import psycopg2
+
+from flask_bcrypt import generate_password_hash, check_password_hash
+def connectDB(host='localhost',database='codejam',user='root',password='1234'):
+    return psycopg2.connect(database="codejam", user = "mridul", password = "1234", host = "127.0.0.1", port = "5432")
+    #return mysql.connector.connect(host=host,database=database,user=user,password=password)
+
+def disconnectDB(conn):
+    conn.close()
+
+def executeDB(conn,sql,values):
+    cursor = conn.cursor()
+    cursor.execute(sql,values)
+    conn.commit()
+    return cursor
+
+def queryDB(conn,sql):
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    try:
+        rows = cursor.fetchall() # fetchone() , fetchmany() , fetchall()
+    except:
+        rows=[]
+    cursor.close()
+    return rows
+
+
+# User Functions
+
+def user_register(username,password,email,bio,dob,ieee_no,branch,sem):
+    c = connectDB()
+    password = generate_password_hash(password)
+    executeDB(c,"insert into members(username,password,email,bio,dob,ieee_no,branch,sem) \
+    values(%s,%s,%s,%s,%s,%s,%s,%s)",(username,password,email,bio,dob,ieee_no,branch,sem))
+    disconnectDB(c)
+    return True
+
+def user_update(username,email,bio,dob,ieee_no,branch,sem):
+    c = connectDB()
+    executeDB(c,"update members set \
+                username=%s, \
+                email=%s, \
+                bio=%s, \
+                dob=%s, \
+                ieee_no=%s, \
+                branch=%s, \
+                sem=%s \
+                where email=%s",(username,email,bio,dob,ieee_no,branch,sem,email))
+    disconnectDB(c)
+    return True
+
+def user_unregister(user_id):
+    c = connectDB()
+    executeDB(c,"delete from members where user_id="+str(user_id),())
+    disconnectDB(c)
+    flush_posts(user_id)
+    flush_follow(user_id)
+    flush_user_likes(user_id)
+    return True
+
+def user_login(email,password):
+    c = connectDB()
+    result = queryDB(c,"select * from members where email='"+email+"'")
+    disconnectDB(c)
+    if len(result)>0:
+        if check_password_hash(result[0][2], password):
+            return result[0]
+        else:
+            return False
+    else:
+        return False
+
+def member_info(user_id):
+    c = connectDB()
+    result = queryDB(c,"select * from members where user_id="+str(user_id))
+    disconnectDB(c)
+    return result[0]
+
+def members_list():
+    c = connectDB()
+    result = queryDB(c,"select user_id,username,email,branch,sem from members")
+    disconnectDB(c)
+    return result
+
+
+
+# Like Unlike lists
+
+def like_post(user_id,post_id):
+    c = connectDB()
+    executeDB(c,"insert into likes values("+str(user_id)+","+str(post_id)+")",())
+    disconnectDB(c)
+    return True
+
+def unlike_post(user_id,post_id):
+    c = connectDB()
+    executeDB(c,"delete from likes where user_id="+str(user_id)+" and post_id="+str(post_id),())
+    disconnectDB(c)
+    return True
+
+def is_liked(user_id,post_id):
+    c = connectDB()
+    result = queryDB(c,"select * from likes where user_id="+str(user_id)+" and post_id="+str(post_id))
+    disconnectDB(c)
+    return len(result)
+
+def get_likes(post_id):
+    c = connectDB()
+    result = queryDB(c,"select count(*) from likes where post_id="+str(post_id))
+    disconnectDB(c)
+    return result[0][0]
+
+
+
+
+
+# Follow unfollow and lists
+
+def is_following(from_id,to_id):
+    c = connectDB()
+    result = queryDB(c,"select * from follow where from_id="+str(from_id)+" and to_id="+str(to_id))
+    disconnectDB(c)
+    return len(result)
+
+def follow(from_id,to_id):
+    c = connectDB()
+    executeDB(c,"insert into follow values("+str(from_id)+","+str(to_id)+")",())
+    disconnectDB(c)
+    return True
+
+def unfollow(from_id,to_id):
+    c = connectDB()
+    executeDB(c,"delete from follow where from_id="+str(from_id) + " and to_id="+str(to_id),())
+    disconnectDB(c)
+    return True
+
+def follower_list(user_id):
+    c = connectDB()
+    result = queryDB(c,"select user_id,username,email,branch,sem from members where user_id in (select from_id from follow where to_id="+str(user_id)+")")
+    disconnectDB(c)
+    return result
+
+def following_list(user_id):
+    c = connectDB()
+    result = queryDB(c,"select user_id,username,email,branch,sem from members where user_id in (select to_id from follow where from_id="+str(user_id)+")")
+    disconnectDB(c)
+    return result
+
+
+
+# Posts info and single post
+
+def add_post(user_id,post_data,group):
+    c = connectDB()
+    executeDB(c,"insert into posts(user_id,wgroup,article,date_time) values(%s,%s,%s,now())",(user_id,group,post_data))
+    disconnectDB(c)
+    return True
+
+def remove_post(post_id):
+    c = connectDB()
+    executeDB(c,"delete from posts where post_id="+post_id,())
+    flush_likes(post_id)
+    disconnectDB(c)
+    return True
+
+def edit_post(article, post_id):
+    c = connectDB()
+    executeDB(c,"update posts set article=%s where post_id="+post_id,(article,))
+    disconnectDB(c)
+    return True
+
+def get_post(post_id):
+    c = connectDB()
+    result = queryDB(c,"select post_id, posts.user_id,username,article,date_time from posts,members where post_id="+post_id+" and posts.user_id=members.user_id order by date_time desc")
+    disconnectDB(c)
+    return result
+
+def post_list():
+    c = connectDB()
+    result = queryDB(c,"select post_id, posts.user_id,username,article,date_time from posts,members where wgroup=0 and posts.user_id=members.user_id order by date_time desc")
+    disconnectDB(c)
+    return result
+
+def post_list_follow(user_id):
+    c = connectDB()
+    result = queryDB(c,"select post_id, posts.user_id,username,article,date_time from posts,members where  wgroup=0 and posts.user_id=members.user_id and posts.user_id in (select to_id from follow where from_id="+user_id+") order by date_time desc")
+    disconnectDB(c)
+    return result
+
+def post_list_user(user_id):
+    c = connectDB()
+    result = queryDB(c,"select post_id, posts.user_id,username,article,date_time from posts,members where posts.user_id=members.user_id and posts.user_id="+user_id+" order by date_time desc");
+    disconnectDB(c)
+    return result
+
+def group_post_list(group):
+    c = connectDB()
+    result = queryDB(c,"select post_id, posts.user_id,username,article,date_time from posts,members where posts.user_id=members.user_id and wgroup="+group+ " order by date_time desc")
+    disconnectDB(c)
+    return result
+
+
+# messaging
+
+def send_message(from_id,to_id,message):
+    c = connectDB()
+    executeDB(c,"insert into messages(from_id,to_id,message_data,date_time) values(%s,%s,%s,now())",(from_id,to_id,message))
+    disconnectDB(c)
+    return True
+
+def get_messages(from_id,to_id):
+    c = connectDB()
+    result = queryDB(c,"select user_id,username,message_data,date_time from messages,members where members.user_id=messages.from_id and ((messages.from_id=%s and messages.to_id=%s) or (messages.from_id=%s and messages.to_id=%s))"%(from_id,to_id,to_id,from_id))
+    print result
+    disconnectDB(c)
+    return result
+
+def clear_messages(from_id,to_id):
+    c = connectDB()
+    executeDB(c,"delete from messages where (from_id=%s and to_id=%s) or (from_id=%s and to_id=%s)",(from_id,to_id,to_id,from_id))
+    disconnectDB(c)
+    return True
+
+
+
+# group Stuff
+
+def get_groups_list():
+    c = connectDB()
+    result = queryDB(c,"select * from groups")
+    disconnectDB(c)
+    return result
+
+def get_group_info(group_id):
+    c = connectDB()
+    result = queryDB(c,"select * from groups where group_id="+group_id)
+    disconnectDB(c)
+    if len(result):
+        return result[0]
+    else:
+        return False
+
+def is_group_member(group_id,user_id):
+    c = connectDB()
+    result = queryDB(c,"select * from groups where members like ('%"+str(user_id)+"%') and group_id="+group_id)
+    disconnectDB(c)
+    return len(result)
+
+def get_group_members(group_id):
+    c = connectDB()
+    result = queryDB(c,"select members from groups where group_id="+group_id)
+    disconnectDB(c)
+    return result[0][0].split(",")
+
+def join_group(group_id,user_id):
+    if (is_group_member(group_id,user_id)==1):
+        return 1
+    else:
+        c = connectDB()
+        members = get_group_members(group_id)
+        members.append(str(user_id))
+        members = ",".join(members)
+        result = executeDB(c,"update groups set members=%s where group_id=%s" ,(members,group_id))
+        disconnectDB(c)
+        return 1
+
+def leave_group(group_id,user_id):
+    if (is_group_member(group_id,user_id)==0):
+        return 1
+    else:
+        c = connectDB()
+        members = get_group_members(group_id)
+        members.remove(unicode(str(user_id)))
+        if len(members)==0:
+            remove_group(group_id)
+            return 0
+        members = ",".join(members)
+        result = executeDB(c,"update  groups  set members=%s where group_id=%s" ,(members,group_id))
+        disconnectDB(c)
+        return 1
+
+def create_group(name,desc,user_id):
+    c = connectDB()
+    rowid = executeDB(c,"insert into groups(name,description,members) values(%s,%s,%s) RETURNING group_id",(name,desc,str(user_id)))
+    rid = rowid.fetchone()[0]
+    disconnectDB(c)
+    return str(rid)
+
+
+
+def remove_group(group_id):
+    c = connectDB()
+    executeDB(c,"delete from groups where group_id=%s",(group_id,))
+    disconnectDB(c)
+    return 1
+
+# Cleanup Tasks
+
+def flush_likes(post_id):
+    c = connectDB()
+    executeDB(c,"delete from likes where post_id="+str(post_id),())
+    disconnectDB(c)
+    return True
+
+def flush_posts(user_id):
+    c = connectDB()
+    executeDB(c,"delete from posts where user_id="+str(user_id),())
+    disconnectDB(c)
+    return True
+
+def flush_follow(user_id):
+    c = connectDB()
+    executeDB(c,"delete from follow where from_id="+str(user_id)+" or to_id="+str(user_id),())
+    disconnectDB(c)
+    return True
+
+def flush_user_likes(user_id):
+    c = connectDB()
+    executeDB(c,"delete from likes where user_id="+str(user_id),())
+    disconnectDB(c)
+    return True
+
+
+
+def init_DB():
+    c = connectDB()
+    sql = '''CREATE TABLE if not exists follow  (
+     from_id   int NOT NULL,
+     to_id   int NOT NULL
+    )'''
+    executeDB(c,sql,())
+    sql = '''CREATE TABLE  likes  (
+     user_id   int NOT NULL,
+     post_id   int NOT NULL
+    ) '''
+    executeDB(c,sql,())
+    sql ='''
+    CREATE TABLE if not exists members  (
+       user_id   serial primary key,
+       username  text NOT NULL,
+       password  text NOT NULL,
+       email  text NOT NULL,
+       bio  text NOT NULL,
+       dob  date NOT NULL,
+       ieee_no  varchar(11) NOT NULL,
+       branch  text NOT NULL,
+       sem  varchar(11) NOT NULL
+      )
+    '''
+    executeDB(c,sql,())
+    sql='''CREATE TABLE if not exists messages  (
+       message_id   serial primary key,
+       from_id   int NOT NULL,
+       to_id   int NOT NULL,
+       message_data  text NOT NULL,
+       date_time  timestamp
+    ) '''
+    executeDB(c,sql,())
+    sql='''CREATE TABLE if not exists posts  (
+       user_id   int NOT NULL,
+       wgroup   int NOT NULL,
+       post_id   serial primary key,
+       article  text NOT NULL,
+       date_time  timestamp
+    )'''
+    executeDB(c,sql,())
+    sql='''create table groups (group_id serial primary key,name text, description text,members text)'''
+    executeDB(c,sql,())
+    disconnectDB(c)
+
+# init_DB()
+
+#print members_list()
+
+# print follower_list('2')
+
+# Tables
+# members
+#     id	 int Auto Increment
+#     username	text
+#     password	text
+#     email	text
+#     bio	text
+#     dob	date
+#     ieee_no	 int
+#     branch	text
+#     sem	 int
+
+# likes
+    # user_id	 int Auto Increment
+    # post_id	 int
+
+# Post
+    # user_id	 int
+    # post_id	 int Auto Increment
+    # post_title	text
+    # article	mediumtext
+    # date_time	timestamp [CURRENT_TIMESTAMP]
+
+
+#USAGE
+# c = connectDB()
+# executeDB(c,"insert into memebers values(a,b,c)")
+# disconnectDB(c)
+
+
+# c = connectDB()
+# result = queryDB(c,"select * from memebers")
+# disconnectDB(c)
